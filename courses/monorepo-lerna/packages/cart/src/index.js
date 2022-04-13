7cb1490b80f59bf03aebf2b69b33ac4c0e07e539
abduvik <contact@abdu.dev>
2022-04-13 20:09:34 +0200

    Update README.md

--- courses/monorepo-lerna/packages/cart/src/index.js
+++ courses/monorepo-lerna/packages/cart/src/index.js
@@ -3,8 +3,6 @@ import { Button } from "@monorepo-lerna/components";
 export const CartPage = () => {
     const container = document.createElement('div');
 
-    console.log('test');
-
     const toCheckout = Button('To Checkout');
     container.appendChild(toCheckout);
 }
--- courses/monorepo-lerna/packages/checkout/src/index.js
+++ courses/monorepo-lerna/packages/checkout/src/index.js
@@ -3,8 +3,6 @@ import { Button } from "@monorepo-lerna/components";
 export const CheckoutPage = () => {
     const container = document.createElement('div');
 
-    console.log('test');
-
     const payButton = Button('Pay');
     container.appendChild(payButton);
 }
