6bec6437df892d590dd7a4a3be6fe28ef71d444d
abduvik <contact@abdu.dev>
2022-11-04 22:51:50 +0100

    Add Support for form submissions

--- courses/react/todo/src/containers/TodoContainer/AddTodoItem/AddTodoItem.tsx
+++ courses/react/todo/src/containers/TodoContainer/AddTodoItem/AddTodoItem.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useState } from "react";
+import { FormEventHandler, MouseEventHandler, useState } from "react";
 import { TextField } from "../../../components/TextField/TextField";
 import { Button } from "../../../components/Button/Button";
 import { withAutoFocus } from "../../../hoc/withAutoFocus";
@@ -12,19 +12,21 @@ export const AddTodoItem = ({ onAddClicked }: any) => {
     setValue(value);
   };
 
-  const onAddClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+  const onAddClick: MouseEventHandler<HTMLButtonElement> &
+    FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
     onAddClicked(value);
     setValue("");
   };
 
   return (
-    <div className="flex">
+    <form className="flex" onSubmit={onAddClick}>
       <div className="mr-1 flex-grow-1">
         <AddTodoItemTextField value={value} onInput={onInput} />
       </div>
       <Button onClick={onAddClick} primary>
         Add
       </Button>
-    </div>
+    </form>
   );
 };
