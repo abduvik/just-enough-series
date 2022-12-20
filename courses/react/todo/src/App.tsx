fc505cefcb4a54272092022c465368ce949adf8c
abduvik <contact@abdu.dev>
2022-12-20 16:20:32 +0100

    Use suspense instead of to do code splitting

--- courses/react/todo/src/App.tsx
+++ courses/react/todo/src/App.tsx
@@ -4,18 +4,28 @@ import React from "react";
 import { PageLayout } from "./partials/PageLayout/PageLayout";
 import { AppStateProvider } from "./store/app.store";
 import "./styles/general.scss";
-import TodoContainer from "./containers/TodoContainer";
-import TodoStatsContainer from "./containers/TodoStatsContainer";
-import AboutContainer from "./containers/AboutContainer";
+import { withSuspense } from "./hoc/withSuspense";
+
+const SuspenseTodoContainer = withSuspense(
+  () => import("./containers/TodoContainer")
+);
+
+const SuspenseTodoStatsContainer = withSuspense(
+  () => import("./containers/TodoStatsContainer")
+);
+
+const SuspenseAboutContainer = withSuspense(
+  () => import("./containers/AboutContainer")
+);
 
 function App() {
   return (
     <AppStateProvider>
       <PageLayout>
         <Routes>
-          <Route path="/" element={<TodoContainer />} />
-          <Route path="/stats" element={<TodoStatsContainer />} />
-          <Route path="/about" element={<AboutContainer />} />
+          <Route path="/" element={SuspenseTodoContainer} />
+          <Route path="/stats" element={SuspenseTodoStatsContainer} />
+          <Route path="/about" element={SuspenseAboutContainer} />
         </Routes>
       </PageLayout>
     </AppStateProvider>
